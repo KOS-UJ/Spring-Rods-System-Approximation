fix the formula of L2 norm

experiments/utils/computational_utils.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def l2_norm(values: Tuple[np.ndarray, np.ndarray], domain: Tuple[np.ndarray, np.ndarray]) -> float:
     base = np.sum((
-        np.sum(np.diff(val) ** 2 * np.diff(dom) ** 2)
+        np.sum((val[:-1]**2 + val[1:]**2 + val[:-1]*val[1:]) * np.diff(dom))
         for val, dom in zip(values, domain)
     ))
     return np.sqrt(base / 3)