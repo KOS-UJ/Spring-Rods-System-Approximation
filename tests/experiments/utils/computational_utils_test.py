00f62eb256e1ae5042922d7c3ac65faf2d5503ad
wprzadka <wiktor.przadka.1@gmail.com>
2023-03-06 14:36:22 +0100

    fix results of L2 norm test

--- tests/experiments/utils/computational_utils_test.py
+++ tests/experiments/utils/computational_utils_test.py
@@ -6,10 +6,9 @@ from experiments.utils.computational_utils import l2_norm
 
 
 @pytest.mark.parametrize(
-    'function, interval, expected',
-    [
-        (lambda x: np.full_like(x, 1), ((-2, -1), (1, 2)), 2),
-        (lambda x: np.where(x > 0, x, 0), ((-1, 0), (0, 1)), 0.5)
+    'function, interval, expected', [
+        (lambda x: np.full_like(x, 1), ((-2, -1), (1, 2)), np.sqrt(2)),
+        (lambda x: np.where(x > 0, x, 0), ((-1, 0), (0, 1)), np.sqrt(1/3))
     ])
 def test_l2_norm(function: Callable, interval: Tuple[Tuple[float, float], Tuple[float, float]], expected: float):
     domain = np.linspace(*(interval[0])), np.linspace(*(interval[1]))
@@ -17,4 +16,4 @@ def test_l2_norm(function: Callable, interval: Tuple[Tuple[float, float], Tuple[
 
     result = l2_norm(values, domain)
 
-    pytest.approx(expected, result)
+    assert result == pytest.approx(expected)
