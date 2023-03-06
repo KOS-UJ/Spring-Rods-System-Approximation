ed4f5e89d84fbc0a1d0913e2b00d48425cfd70b8
wprzadka <wiktor.przadka.1@gmail.com>
2023-03-06 18:57:02 +0100

    test scalar product computation

--- tests/spring_rods_system_setup_test.py
+++ tests/spring_rods_system_setup_test.py
@@ -1,5 +1,7 @@
 import pytest
 import numpy as np
+from scipy.integrate import quad
+
 from spring_rods_system_setup import SpringRodsSystemSetup
 
 
@@ -25,3 +27,19 @@ class TestSpringRodsSystemSetup:
         result = self.system.effect_of_spring(displacements)
 
         assert result == pytest.approx(spring_reaction)
+
+    @pytest.mark.parametrize('displacement_func, body_func', [
+        (lambda x: np.where(x < 0, (x + 10), (x-10)), lambda x: np.full_like(x, 1)),
+        (lambda x: np.where(x < 0, (x + 10)**2, (x-10)**2), lambda x: np.where(x < 0, -1, 1))
+    ])
+    def test_effect_of_body_forces(self, displacement_func, body_func):
+        self.system.set_body_forces(body_func)
+        domain = self.system.domain
+        displacements = (displacement_func(domain[0]), displacement_func(domain[1]))
+
+        result = self.system.effect_of_body_forces(displacements)
+
+        left_rod_int = quad(lambda x: displacement_func(x) * body_func(x), domain[0][0], domain[0][-1])[0]
+        right_rod_int = quad(lambda x: displacement_func(x) * body_func(x), domain[1][0], domain[1][-1])[0]
+        expected = left_rod_int + right_rod_int
+        assert result == pytest.approx(expected)
