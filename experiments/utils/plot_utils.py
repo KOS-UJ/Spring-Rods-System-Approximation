b0e7feedeb43e6f536090f5183dbb4f1ec786e71
wprzadka <wiktor.przadka.1@gmail.com>
2023-03-21 21:20:59 +0100

    make stiffness colors optional

--- experiments/utils/plot_utils.py
+++ experiments/utils/plot_utils.py
@@ -14,7 +14,8 @@ def plot_displacements_and_stress(
         parameters_space: list,
         parameter_name: str,
         limits: Tuple[float, float],
-        path: str
+        path: str,
+        color_stresses: bool = False
 ):
     normalize = colors.Normalize(vmin=limits[0], vmax=limits[1])
     cmap = sns.diverging_palette(255, 15, s=100, l=60, sep=1, center="dark", as_cmap=True)
@@ -29,27 +30,37 @@ def plot_displacements_and_stress(
 
         for side in (0, 1):
             new_positions = model.domain[side] + displacements[side]
-            for p_idx, _ in enumerate(new_positions[:-1]):
-                plt.plot(
-                    [new_positions[p_idx], new_positions[p_idx + 1]],
-                    [-idx, -idx],
-                    color=cmap(normalize(stresses[side][p_idx])),
-                    linewidth=4
-                )
+            if color_stresses:
+                for p_idx, _ in enumerate(new_positions[:-1]):
+                    plt.plot(
+                        [new_positions[p_idx], new_positions[p_idx + 1]],
+                        [-idx, -idx],
+                        color=cmap(normalize(stresses[side][p_idx])),
+                        linewidth=4
+                    )
+            else:
+                plt.plot([new_positions[0], new_positions[-1]], [-idx, -idx], color='black', linewidth=2)
 
     plt.axvline(x=0, color='gray', linestyle='--')
     plt.axvline(x=-model.spring_len / 2, color='gray', linestyle=':')
     plt.axvline(x=model.spring_len / 2, color='gray', linestyle=':')
 
-    plt.figtext(0.15, 0.03, 'a', horizontalalignment='center', verticalalignment='top')
-    plt.figtext(0.285, 0.03, '-l', horizontalalignment='center', verticalalignment='top')
-    plt.figtext(0.58, 0.03, 'l', horizontalalignment='center', verticalalignment='top')
-    plt.figtext(0.72, 0.03, 'b', horizontalalignment='center', verticalalignment='top')
+    if color_stresses:
+        plt.figtext(0.15, 0.03, 'a', horizontalalignment='center', verticalalignment='top')
+        plt.figtext(0.285, 0.03, '-l', horizontalalignment='center', verticalalignment='top')
+        plt.figtext(0.58, 0.03, 'l', horizontalalignment='center', verticalalignment='top')
+        plt.figtext(0.72, 0.03, 'b', horizontalalignment='center', verticalalignment='top')
+    else:
+        plt.figtext(0.16, 0.03, 'a', horizontalalignment='center', verticalalignment='top')
+        plt.figtext(0.34, 0.03, '-l', horizontalalignment='center', verticalalignment='top')
+        plt.figtext(0.69, 0.03, 'l', horizontalalignment='center', verticalalignment='top')
+        plt.figtext(0.87, 0.03, 'b', horizontalalignment='center', verticalalignment='top')
 
     plt.ylabel(parameter_name)
     plt.xlabel('Position')
 
-    sm = plt.cm.ScalarMappable(cmap=cmap, norm=normalize)
-    plt.gcf().colorbar(sm, aspect=50)
+    if color_stresses:
+        sm = plt.cm.ScalarMappable(cmap=cmap, norm=normalize)
+        plt.gcf().colorbar(sm, aspect=50)
 
     plt.savefig(path, bbox_inches='tight')
